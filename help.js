5742275810d3beba3cb589981b0f40ddcddc7494
vadimgb <vadimgb@yandex.ru>
2021-06-03 21:27:13 +0700

    ups

--- help.js
+++ help.js
@@ -1,51 +1,22 @@
 const fetch = require('node-fetch')
 require('dotenv').config()
-const token1 ='360ce9ccde4181ec6a3a55de94dfe088c08faad4'
-deleteRepo(token1, 'vadimgb1')
-//getRepo(token1, 'vadimgb1')
+;(async ()=>console.log(await
+existsRepo('cs204', process.env.TSPU_TOKEN, 'vadimgb2')))();
 
-async function deleteRepo(org_token,  name)
+async function existsRepo(org_name,  token, username)
 {
-	const body = {
-		'acsept': 'application/vnd.github.v3+json',
-		'owner':'tspu1',
-		'name': name
-	}
-
-	const url1 = `https://api.github.com/repos/tspu1/${name}`
-	try
-	{
-		const res = await fetch(url1, 
-			{
-				method: 'delete',
-				body: JSON.stringify(body), 
-				headers:
-				{
-					"Accept": "application/vnd.github.v3+json",
-					"Authorization": `token ${org_token}`
-				}
-			})
-	//	const repos = await res.json()
-		console.log(res)
-	}
-	catch(err)
-	{
-		console.log(err)
-	}
-}
-
-async function getRepo(org_token, name)
-{
-	const url1 = `https://api.github.com/repos/tspu1/${name}`
+	const url1 = `https://api.github.com/repos/${org_name}/${username}`;
+	console.log(url1);
 	const res = await fetch(url1, 
 		{
-			method: 'get',
+			method: "get",
 			headers:
 			{
-				"Authorization": `token ${org_token}`
+				"Accept": "application/vnd.github.v3+json",
+				"Authorization": `token ${token}`
 			}
 		})
-	const repos = await res.json()
-	console.log(repos)
+
+	return res.status == 200;
 }
 
