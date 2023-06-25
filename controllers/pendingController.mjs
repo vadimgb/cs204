3852b524a2b457805cb5487c75904354422c1fbd
Vadim G.B <vadimgb1@gmail.com>
2023-06-25 22:30:17 +0700

    pending

--- controllers/pendingController.mjs
+++ controllers/pendingController.mjs
@@ -7,14 +7,6 @@ async function index(req, res)
 
 async function api_grades(req, res) 
 {
-	const sql1 = `select select1.firstname, select1.lastname, select1.house, select1.is_active, 
-		gradebook.grade, gradebook.url, gradebook.notes, 
-		gradebook.is_done, gradebook.is_checked,  gradebook.timestamp_done, gradebook.id_character, gradebook.id_problemset
-		from (select characters.id, firstname, lastname,  houses.house, houses.is_active from  characters 
-		join houses on characters.id_house = houses.id) select1
-		left join gradebook on select1.id = gradebook.id_character where 
-		gradebook.is_done = true and gradebook.is_checked = false and is_active order by house;` 
-
 	const sql2 =`select ch.house, ch.lastname, ch.firstname, 
 		problemset.name, problemset.description, 
 		g.id_problemset, g.grade, g.is_done, g.url, g.id_character, g.notes
